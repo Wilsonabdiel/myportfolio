15abc95954c5006ff22dccdc7ab245508781ee9a
Wilsonabdiel <wilsonabdiel86@gmail.com>
2022-09-06 14:35:44 +0100

    New changes  attempting tohost

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -126,6 +127,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
