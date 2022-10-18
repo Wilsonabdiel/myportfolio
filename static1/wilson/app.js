b10cbcd9262545cdd8acbe391a3dbb400fa873a4
Abdiel <wilsonabdiel86@gmail.com>
2022-10-18 16:07:12 +0100

    Responsive to mobiles

--- static1/wilson/app.js
+++ static1/wilson/app.js
@@ -1,42 +1,15 @@
-var clickPressed = false;
-var startPoint = {x: 0, y: 0};
-var $donut = $('.donut');
-var rotationObject = {
-  x: 0,
-  y: 0,
-  z: 0
-};
 
-function mouseMoveHandler(e){
-  if(e.clientX > startPoint.x){
-    rotationObject.y = rotationObject.y >= 180 ? 180 : rotationObject.y+1;
-  } else if(e.clientX < startPoint.x){
-    rotationObject.y = rotationObject.y <= -180 ? -180 : rotationObject.y-1;
+function burgeropen (){
+  document.querySelector('#navel').innerHTML= `
+  <ul class="list-group">
+  <li class="list-group-item active"><a href="index.html">Abdiel</a></li>
+  <li class="list-group-item active"><a href="about.html">About</a></li>
+  <li class="list-group-item active"><a href="skills.html>Skills</a></li>
+  <li class="list-group-item active"><a href="blog.html">Blog</a></li>
+  <li class="list-group-item active"><a href="">Projects</a></li>
+  
+  
+ 
+</ul>
+  `;
   }
-  
-  if(e.clientY > startPoint.y){
-    rotationObject.x = rotationObject.x >= 180 ? 180 : rotationObject.x+1;
-  } else if(e.clientY < startPoint.y){
-    rotationObject.x = rotationObject.x <= -180 ? -180 : rotationObject.x-1;
-  }
-  
-  startPoint.x = e.clientX;
-  startPoint.y = e.clientY;
-    
-  $donut.css({
-    'transform': 'rotateX('+rotationObject.x+'deg) rotateY('+rotationObject.y+'deg)'
-  });
-  
-}
-
-$(document).on('mousedown', function(e){
-  clickPressed = true;
-  startPoint.x = e.clientX;
-  startPoint.y = e.clientY;
-
-  $(document).on('mousemove', mouseMoveHandler);
-});
-
-$(document).on('mouseup', function(e){
-  $(document).off('mousemove', mouseMoveHandler);
-});
